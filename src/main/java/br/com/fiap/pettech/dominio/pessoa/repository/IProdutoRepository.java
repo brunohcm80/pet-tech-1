7a6e74e648efa205e11547b2bf6e2977eb4213f0
Bruno Henrique Cavalcanti Mello <brunohcm@gmail.com>
2023-06-07 22:04:23 -0300

    Inclusão da Anotação @Repository

--- src/main/java/br/com/fiap/pettech/dominio/pessoa/repository/IProdutoRepository.java
+++ src/main/java/br/com/fiap/pettech/dominio/pessoa/repository/IProdutoRepository.java
@@ -2,8 +2,10 @@ package br.com.fiap.pettech.dominio.pessoa.repository;
 
 import br.com.fiap.pettech.dominio.pessoa.produto.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface IProdutoRepository extends JpaRepository<Produto, UUID>{
 }
--- src/main/java/br/com/fiap/pettech/dominio/pessoa/service/ProdutoService.java
+++ src/main/java/br/com/fiap/pettech/dominio/pessoa/service/ProdutoService.java
@@ -13,7 +13,6 @@ import java.util.UUID;
 @Service
 public class ProdutoService {
 
-    @Qualifier("produtoRepository")
     @Autowired
     private IProdutoRepository repo;
 
