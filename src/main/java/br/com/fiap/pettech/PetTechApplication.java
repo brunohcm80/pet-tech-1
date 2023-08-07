ee8aaf456e0550b809af90dfa14b3e6eb5484f48
Bruno Henrique Cavalcanti Mello <brunohcm@gmail.com>
2023-08-07 00:18:52 -0300

    Refatoração API de Pessoas
    Criação da Controller, Repository utilizando JDBC

--- src/main/java/br/com/fiap/pettech/PetTechApplication.java
+++ src/main/java/br/com/fiap/pettech/PetTechApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetTechApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PetTechApplication.class, args);
     }
